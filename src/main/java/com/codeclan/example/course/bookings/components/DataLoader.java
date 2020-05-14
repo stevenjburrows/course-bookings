a59b471e3c8fdf9b5803e20525522bce6f75d546
steven <steven@burrows.plus.com>
2020-05-14 11:57:48 +0100

    added seed data

--- src/main/java/com/codeclan/example/course/bookings/components/DataLoader.java
+++ src/main/java/com/codeclan/example/course/bookings/components/DataLoader.java
@@ -1,5 +1,8 @@
 package com.codeclan.example.course.bookings.components;
 
+import com.codeclan.example.course.bookings.models.Booking;
+import com.codeclan.example.course.bookings.models.Course;
+import com.codeclan.example.course.bookings.models.Customer;
 import com.codeclan.example.course.bookings.repositories.BookingRepository;
 import com.codeclan.example.course.bookings.repositories.CourseRepository;
 import com.codeclan.example.course.bookings.repositories.CustomerRepository;
@@ -23,6 +26,41 @@ public class DataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+
+        Course java = new Course("Java", "Edinburgh", 4);
+        courseRepository.save(java);
+        Course python = new Course("Python", "Livingston", 2);
+        courseRepository.save(python);
+        Course ruby = new Course("Ruby", "Glasgow", 5);
+        courseRepository.save(ruby);
+
+
+        Customer john = new Customer("John","Stirling",32);
+        customerRepository.save(john);
+
+        Customer ian = new Customer("Ian","Belfast",35);
+        customerRepository.save(ian);
+
+        Customer rob = new Customer("Rob","Armadale",29);
+        customerRepository.save(rob);
+
+        Booking booking1 = new Booking("23-04-2020", java, john);
+        bookingRepository.save(booking1);
+
+        Booking booking2 = new Booking("23-04-2020", python, ian);
+        bookingRepository.save(booking2);
+
+        Booking booking3 = new Booking("23-04-2020", java, rob);
+        bookingRepository.save(booking3);
+
+        Booking booking4 = new Booking("23-04-2020", ruby, john);
+        bookingRepository.save(booking4);
+
+
+
+
+
+
 
     }
 }
